cd10a0916bd04b9cf0074206b7ce0f9653db8e4d
XoanCarreira <154058163+XoanCarreira@users.noreply.github.com>
2025-01-02 18:32:12 +0100

    completo textos

--- wp-content/uploads/uag-plugin/assets/0/uag-js-70.js
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-70.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-a7654af6', {"layout":"number","heading":"Years Of Experience","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":20,"totalNumber":350,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+	UAGBCounter.init( '.uagb-block-a7654af6', {"layout":"number","heading":"A\u00f1os de experiencia","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":20,"totalNumber":350,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-38e547d2', {"layout":"number","heading":"Happy Customers","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":350,"totalNumber":350,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+	UAGBCounter.init( '.uagb-block-38e547d2', {"layout":"number","heading":"Entrega y dedicaci\u00f3n","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":100,"totalNumber":1000,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-eb510c9f', {"layout":"number","heading":"Customer Satisfaction","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":98,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+	UAGBCounter.init( '.uagb-block-eb510c9f', {"layout":"number","heading":"Clientes satisfechos","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":99,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-d32efc81' );
